43d7990401aeaab72426be4c31af74dbc49b5cb4
dale tan <dale@1stdibs.com>
2013-04-12 18:36:37 -0400

    FINDING-175 making accordion plugin more customizable

--- jquery.simple-accordion.js
+++ jquery.simple-accordion.js
@@ -28,65 +28,172 @@
  *      $('.my-class').simpleAccordionList()
  */
 (function ($) {
+    "use strict";
     $.fn.simpleAccordionList = function (settings) {
-        var defaults = {
+        var api = this,
+            defaults = {
                 defaultTrigger: null,
-                allowAllOpened: false
+                allowAllOpened: false,
+                allowAnimation: true,
+                classes: {
+                    master: 'master',
+                    masterTrigger: 'master-accordion-list-trigger',
+                    container: 'accordion-list',
+                    listTrigger: 'accordion-list-trigger',
+                    listHead: 'accordion-list-head',
+                    listItems: 'accordion-list-items'
+                },
+                stateClasses: {
+                    masterExpanded: 'master-expanded'
+                }
+                // _classes and _stateClasses properties get created with
+                // respective classes that start with periods
             },
-            options = $.extend(true, defaults, settings),
-            allItems = $('.accordion-list-items');
+            options,
+            $allItems;
 
+
+        function init(settings) {
+            options = $.extend(true, defaults, settings);
+            createClassStrings(["classes", "stateClasses"]);
+            $allItems = $(options._classes.listItems);
+            api.setup();
+        }
+
+        /**
+         *
+         * @param key {String|Array} Either a string of the key to use or an array of keys to use to create actual string classes
+         */
+        function createClassStrings(key) {
+            if (!key) {
+                return;
+            }
+            var startRegex = /^\./,
+                tmpClass, obj;
+
+            /**
+             * Creates string classes with correct formatting from a reference class (className => .className) and adds them to antother object for reference.
+             * If the `className` already contains a leading period
+             * @param keyName {String} The key name for the object to use based on the `options` object
+             * @returns {object} The newly created strings object
+             */
+            function createStrings(keyName) {
+                var obj = options[keyName];
+                if (!obj) {
+                    return;
+                }
+                var _obj = options['_' + keyName] = {}, klass;
+                for (klass in obj) {
+                    if (obj.hasOwnProperty(klass)) {
+                        tmpClass = obj[klass];
+                        if (!startRegex.test(tmpClass)) {
+                            tmpClass = '.' + tmpClass;
+                        }
+                        _obj[klass] = tmpClass;
+                    }
+                }
+                return _obj;
+            }
+
+            if (Object.prototype.toString.call(key) === '[object Array]') {
+                for (var i = 0, len = key.length; i < len; i++) {
+                    createStrings(key[i]);
+                }
+            } else {
+                createStrings(key);
+            }
+
+
+        }
+
+        this.setup = function () {
+
+            api.find(options._classes.masterTrigger).click(function () {
+                var $this = $(this),
+                    master = $this.parents(options._classes.master),
+                    head = $this.parent(options._classes.listHead);
+
+                head.toggleClass('master-expanded');
+
+                if (master.length && master.is(':visible')) {
+                    master.find('.accordion-list').slideToggle();
+                }
+                return false;
+            });
+
+            api.find(options._classes.listTrigger).click(function (e) {
+                var $this = $(this),
+                    $heads = $(options._classes.listHead),
+                    $myHead = $this.parent(options._classes.listHead),
+                    $items = $this.siblings(options._classes.listItems);
+
+                if (e.target) {
+                    if (e.target.nodeName.toLowerCase() === 'a') {
+                        // this takes care of any links nested inside the trigger
+                        // since you cannot nest anchor tags inside anchor tags, this works out
+                        // solves for the "clear" link
+                        return;
+                    }
+                }
+
+                if (!options.allowAllOpened) {
+                    $allItems.slideUp();
+                }
+
+                $heads
+                    .removeClass('is-expanded')
+                    .addClass('is-collapsed');
+
+                if ($items.is(':hidden')) {
+                    $myHead
+                        .removeClass('is-collapsed')
+                        .addClass('is-expanded');
+                    if (!options.allowAnimation) {
+                        // fire this state if on mobile
+                        // otherwise animation looks crappy
+                        // no animation on the initial open
+                        console.log('showing');
+                        $items.show(function () {
+                            console.log("done showing")
+                        });
+                    } else {
+                        console.log('sliding down');
+                        $items.slideDown(function () {
+                            console.log("done sliding down")
+                        });
+                    }
+                } else {
+                    if (!options.allowAnimation) {
+                        // fire this state if on mobile
+                        // otherwise animation looks crappy
+                        // no animation on the initial open
+                        console.log('hididng');
+                        $items.hide(function () {
+                            console.log('donee hiding')
+                        });
+                    } else {
+                        console.log('sliding up');
+                        $items.slideUp(function () {
+                            console.log("done slidign up");
+                        });
+                    }
+                }
+                return false;
+            });
+
+            if (options.defaultTrigger) {
+                $(options.defaultTrigger).click();
+            }
+        };
 
         this.fireEvent = function (event) {
-            if ($.publish) { // based on the simple pub/sub found in core/dibs.js
+            if ($.publish && event) { // based on the simple pub/sub found in core/dibs.js
                 // only want arguments after the `event` argument
                 $.publish(event, [].slice.call(arguments, 1));
             }
         };
 
-        this.find('.master-accordion-list-trigger').click(function () {
-            var master = $(this).parents('.master'),
-                head = $(this).parent('.accordion-list-head');
-
-            head.toggleClass('master-expanded');
-
-            if (master.length && master.is(':visible')) {
-                master.find('.accordion-list').slideToggle();
-            }
-            return false;
-        });
-
-        this.find('.accordion-list-trigger').click(function () {
-            var heads = $('.accordion-list-head'),
-                myHead = $(this).parent('.accordion-list-head'),
-                items = $(this).siblings('.accordion-list-items');
-
-            if (!options.allowAllOpened) {
-                allItems.slideUp();
-            }
-
-            heads
-                .removeClass('is-expanded')
-                .addClass('is-collapsed');
-
-            if (items.is(':hidden')) {
-                myHead.addClass('is-expanded');
-                if (options.defaultTrigger) {
-                    // no animation on the initial open
-                    options.defaultTrigger = null;
-                    items.show();
-                } else {
-                    items.slideDown();
-                }
-            }
-            return false;
-        });
-
-        if (options.defaultTrigger) {
-            $(options.defaultTrigger).click();
-        }
-
-
+        init(settings);
 
         return this;
     };
