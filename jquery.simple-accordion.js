d2eced4f44b3c6622d651a45eef4005bd5f9be7d
dale tan <dale@1stdibs.com>
2013-04-16 21:14:12 -0400

    FINDING-175 updating according to be more robust and allow for nested accordion lists

--- jquery.simple-accordion.js
+++ jquery.simple-accordion.js
@@ -29,7 +29,133 @@
  */
 (function ($) {
     "use strict";
-    $.fn.simpleAccordionList = function (settings) {
+
+    /**
+     *
+     * @param key {String|Array} Either a string of the key to use or an array of keys to use to create actual string classes
+     */
+    function createClassStrings(options, key) {
+        if (!key) {
+            return;
+        }
+        var startRegex = /^\./,
+            tmpClass, obj;
+
+        /**
+         * Creates string classes with correct formatting from a reference class (className => .className) and adds them to antother object for reference.
+         * If the `className` already contains a leading period
+         * @param keyName {String} The key name for the object to use based on the `options` object
+         * @returns {object} The newly created strings object
+         */
+        function createStrings(keyName) {
+            var obj = options[keyName];
+            if (!obj) {
+                return;
+            }
+            var _obj = options['_' + keyName] = {}, klass;
+            for (klass in obj) {
+                if (obj.hasOwnProperty(klass)) {
+                    tmpClass = obj[klass];
+                    if (!startRegex.test(tmpClass)) {
+                        tmpClass = '.' + tmpClass;
+                    }
+                    _obj[klass] = tmpClass;
+                }
+            }
+            return _obj;
+        }
+
+        if (Object.prototype.toString.call(key) === '[object Array]') {
+            for (var i = 0, len = key.length; i < len; i++) {
+                createStrings(key[i]);
+            }
+        } else {
+            createStrings(key);
+        }
+
+        return options;
+
+    }
+
+    $.fn.simpleAccordionList = function(settings) {
+        var api = this,
+            defaults = {
+                allowAllOpened: false,
+                allowAnimation: false,
+                classes: {
+                    list: 'accordion-list',
+                    trigger: 'accordion-list-trigger',
+                    listItems: 'accordion-list-items'
+                }
+            },
+            options,
+            $allItems;
+
+        function init(settings) {
+            options = $.extend(true, defaults, settings);
+            console.log(options);
+            options = createClassStrings(options, ["classes", "stateClasses"]);
+            $allItems = $(options._classes.listItems);
+            api.setup();
+        }
+
+        this.setup = function () {
+            var $trigger = $(options._classes.trigger),
+                $par = $trigger.closest(options._classes.list);
+
+            $par.on('click', $trigger, function (e) {
+                e.preventDefault();
+                var $this = $(this),
+                    $items = $this.find('.accordion-list-items');
+
+                if ($par.children($items).length) {
+                    console.log('we have children');
+                } else {
+                    console.log('NOOOOOO')
+                }
+
+
+                console.log(this);
+                console.log('hi', $items);
+
+                if (!options.allowAllOpened) {
+//                    $allItems
+//                        .removeClass('is-opened')
+//                        .addClass('is-closed')
+//                        .slideUp();
+                }
+
+                if ($items.hasClass('is-closed') || (!$items.hasClass('is-closed') && !$items.hasClass('is-opened'))) {
+                    console.log("opening")
+                    $this.addClass('is-expanded');
+                    $items
+                        .slideDown(function () {
+                            $items
+                                .removeClass('is-closed')
+                                .addClass('is-opened');
+                        });
+                } else {
+                    console.log("closing")
+                    $this.removeClass('is-expanded');
+                    $items
+                        .slideUp(function () {
+                            $items
+                                .removeClass('is-opened')
+                                .addClass('is-closed');
+                        });
+                }
+                return false;
+            });
+
+        };
+
+        init(settings);
+
+        return this;
+
+    };
+
+    $.fn.simpleAccordionList_old = function (settings) {
         var api = this,
             defaults = {
                 defaultTrigger: null,
@@ -41,7 +167,11 @@
                     container: 'accordion-list',
                     listTrigger: 'accordion-list-trigger',
                     listHead: 'accordion-list-head',
-                    listItems: 'accordion-list-items'
+                    listItems: 'accordion-list-items',
+                    measurements: 'accordion-list-measurements' // a container that just helps get the height for css3 animations
+                },
+                elements: {
+                    triggerTag: 'span' // this case is when a link is inside the accordion trigger
                 },
                 stateClasses: {
                     masterExpanded: 'master-expanded'
@@ -60,51 +190,6 @@
             api.setup();
         }
 
-        /**
-         *
-         * @param key {String|Array} Either a string of the key to use or an array of keys to use to create actual string classes
-         */
-        function createClassStrings(key) {
-            if (!key) {
-                return;
-            }
-            var startRegex = /^\./,
-                tmpClass, obj;
-
-            /**
-             * Creates string classes with correct formatting from a reference class (className => .className) and adds them to antother object for reference.
-             * If the `className` already contains a leading period
-             * @param keyName {String} The key name for the object to use based on the `options` object
-             * @returns {object} The newly created strings object
-             */
-            function createStrings(keyName) {
-                var obj = options[keyName];
-                if (!obj) {
-                    return;
-                }
-                var _obj = options['_' + keyName] = {}, klass;
-                for (klass in obj) {
-                    if (obj.hasOwnProperty(klass)) {
-                        tmpClass = obj[klass];
-                        if (!startRegex.test(tmpClass)) {
-                            tmpClass = '.' + tmpClass;
-                        }
-                        _obj[klass] = tmpClass;
-                    }
-                }
-                return _obj;
-            }
-
-            if (Object.prototype.toString.call(key) === '[object Array]') {
-                for (var i = 0, len = key.length; i < len; i++) {
-                    createStrings(key[i]);
-                }
-            } else {
-                createStrings(key);
-            }
-
-
-        }
 
         this.setup = function () {
 
@@ -116,7 +201,7 @@
                 head.toggleClass('master-expanded');
 
                 if (master.length && master.is(':visible')) {
-                    master.find('.accordion-list').slideToggle();
+                    master.find(options._classes.accordionList).slideToggle();
                 }
                 return false;
             });
@@ -126,13 +211,15 @@
                     $heads = $(options._classes.listHead),
                     $myHead = $this.parent(options._classes.listHead),
                     $items = $this.siblings(options._classes.listItems);
-
+console.log('e target: ', e.target);
                 if (e.target) {
-                    if (e.target.nodeName.toLowerCase() === 'a') {
-                        // this takes care of any links nested inside the trigger
-                        // since you cannot nest anchor tags inside anchor tags, this works out
-                        // solves for the "clear" link
-                        return;
+                    if ($(e.target).parent(options.elements.triggerTag).length) {
+                        if (e.target.nodeName.toLowerCase() === 'a') {
+                            // this takes care of any links nested inside the trigger
+                            // since you cannot nest anchor tags inside anchor tags, this works out
+                            // solves for the "clear" link
+                            return;
+                        }
                     }
                 }
 
@@ -144,7 +231,8 @@
                     .removeClass('is-expanded')
                     .addClass('is-collapsed');
 
-                if ($items.is(':hidden')) {
+                var $heightWrapper = $items.find('.accordion-list-measurements');
+                if ($items.is(':hidden') || parseInt($items.css('height'), 10) === 0) {
                     $myHead
                         .removeClass('is-collapsed')
                         .addClass('is-expanded');
@@ -152,26 +240,52 @@
                         // fire this state if on mobile
                         // otherwise animation looks crappy
                         // no animation on the initial open
-                        $items.css({
-                            'display': 'block'
-                        });
+//                        $items.css({
+//                            'display': 'block'
+//                        });
+
+                        console.log('height: ', $items.find('.accordion-list-measurements').height());
+
+
+                        if ($heightWrapper.length) {
+                            $items.height($heightWrapper.outerHeight());
+                        }
+
+                        $items
+                            .removeClass('is-closed')
+                            .addClass('animate is-opened');
                     } else {
-                        $items.slideDown(function () {
-                            console.log("done sliding down")
-                        });
+                        $items
+                            .removeClass('is-closed')
+                            .addClass('is-opened')
+                            .slideDown(function () {
+                                console.log("done sliding down")
+                            });
                     }
                 } else {
                     if (!options.allowAnimation) {
+                        console.log(' close height: ', $items.height());
                         // fire this state if on mobile
                         // otherwise animation looks crappy
                         // no animation on the initial open
-                        $items.css({
-                            'display': 'none'
-                        });
+//                        $items.css({
+//                            'display': 'none'
+//                        });
+
+                        if ($heightWrapper.length) {
+                            $items.height(0);
+                        }
+
+                        $items
+                            .removeClass('is-opened')
+                            .addClass('animate is-closed');
                     } else {
-                        $items.slideUp(function () {
-                            console.log("done slidign up");
-                        });
+                        $items
+                            .removeClass('is-closed')
+                            .addClass('is-opened')
+                            .slideUp(function () {
+                                console.log("done slidign up");
+                            });
                     }
                 }
                 return false;
